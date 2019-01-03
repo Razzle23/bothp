610ba6f56e6f418798aa70cf55d67de2c1629a06
Razzle23 <46345507+Razzle23@users.noreply.github.com>
2019-01-03 15:30:07 +0300

    Update index.js

--- index.js
+++ index.js
@@ -24,27 +24,7 @@ var client = new tmi.client(options);
 client.connect();
 client.on('chat', function(channel, userstate, message, self){
         if(message.includes("@"+process.env.USERNAME)){ // checking if SUSI is tagged
-                var u = message.split("@" + process.env.USERNAME + " ");
-                // Setting options to make a successful call to SUSI API
-                var options1 = {
-                        method: 'GET',
-                        url: 'http://api.susi.ai/susi/chat.json',
-                        qs:
-                        {
-                                timezoneOffset: '-300',
-                                q: u[1]
-                        }
-                };
-                request(options1, function(error, response, body) {
-                        if (error) throw new Error(error);
-                        if((JSON.parse(body)).answers[0])
-                                ans = userstate['display-name'] + " " + (JSON.parse(body)).answers[0].actions[0].expression;
-                        else
-                                ans = userstate['display-name'] + " Sorry, I could not understand what you just said."
-                
-                        client.action(userChannel, ans);
-                });
-        }
+             client.action(userChannel, `Hi, I'm SUSI. use @${process.env.USERNAME} fdsfdsgs.`);
 });
 
 client.on('connected', function(address, port){
